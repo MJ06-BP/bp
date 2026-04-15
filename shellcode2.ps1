49af232e227f62c1304fa7f6a3c146a18e7b2d17
MJ06-BP <milanjansen23706@gmail.com>
2026-04-15 22:04:21 +0200

    Update shellcode2.ps1

--- shellcode2.ps1
+++ shellcode2.ps1
@@ -6,16 +6,16 @@ if (-not [Environment]::Is64BitProcess) {
 }
 
 Clear-Host
-    Write-Host ""
-    Write-Host "  ╔══════════════════════════════════════╗" -ForegroundColor Cyan
-    Write-Host "  ║                                      ║" -ForegroundColor Cyan
-    Write-Host "  ║       NIGGER BYPASS  v6.7            ║" -ForegroundColor Yellow
-    Write-Host "  ║       Gemaakt door: MJBP <3          ║" -ForegroundColor White
-    Write-Host "  ║                                      ║" -ForegroundColor Cyan
-    Write-Host "  ╚══════════════════════════════════════╝" -ForegroundColor Cyan
-    Write-Host ""
-    Write-Host ""
-    Write-Host ""
+Write-Host ""
+Write-Host "  ╔══════════════════════════════════════╗" -ForegroundColor Cyan
+Write-Host "  ║                                      ║" -ForegroundColor Cyan
+Write-Host "  ║       NIGGER BYPASS  v6.7            ║" -ForegroundColor Yellow
+Write-Host "  ║       Gemaakt door: MJBP <3          ║" -ForegroundColor White
+Write-Host "  ║                                      ║" -ForegroundColor Cyan
+Write-Host "  ╚══════════════════════════════════════╝" -ForegroundColor Cyan
+Write-Host ""
+Write-Host ""
+Write-Host ""
 Write-Host "[+] Zoeken naar Chrome processen..." -ForegroundColor Cyan
 
 $edgeProcesses = Get-Process -Name "chrome" -ErrorAction SilentlyContinue
